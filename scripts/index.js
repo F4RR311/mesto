65983df2b7330a7829869ec5e3f843c5c26be4af
F4RR311 <valery.leo2016@yandex.ru>
2022-01-16 21:58:53 +0300

    popup script

---
+++ scripts/index.js
@@ -0,0 +1,19 @@
+const profileOpenPopupButton = document.querySelector('.profile__edit-button');
+const popup = document.querySelector('.popup');
+const popupCloseButton = document.querySelector('.popup__close');
+
+function openPopup(){
+    popup.classList.add('popup__opened')
+}
+function closePopup(){
+    popup.classList.remove('popup__opened')
+}
+
+
+profileOpenPopupButton.addEventListener('click', openPopup);
+popupCloseButton.addEventListener('click', closePopup);
+popup.addEventListener('click', function (e){
+    if(e.target === e.currentTarget){
+        closePopup();
+    }
+})
