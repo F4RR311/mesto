f8ea809cf61447b8be3221be2c0046f9eecd4740
F4RR311 <valery.leo2016@yandex.ru>
2022-01-28 23:31:30 +0300

    new chapter

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,3 @@
-const initialCards = [
-    {
-        name: 'Архыз',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-        name: 'Челябинская область',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-        name: 'Иваново',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-        name: 'Камчатка',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-        name: 'Холмогорский район',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-        name: 'Байкал',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
-];
-
 let profileOpenPopupButton = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
 let popupCloseButton = document.querySelector('.popup__close');
@@ -35,35 +8,17 @@ let profileDescription = document.querySelector('.profile__description');
 let formElement = document.querySelector('.popup__form');
 
 
-let cardAddOpenPopupButton = document.querySelector('.profile__add-button');
-let cardClosePopupButton = document.querySelector('.popup__close_card');
-let popupCard = document.querySelector('.popup_card');
-
-
 function openPopup() {
     popup.classList.add('popup_opened');
     nameInput.value = profileTitle.textContent;
     jobInput.value = profileDescription.textContent;
 }
 
-function openPopupCard() {
-    popupCard.classList.add('popup_opened');
-
-}
-
-
 function closePopup() {
     popup.classList.remove('popup_opened');
-    popupCard.classList.remove('popup_opened');
-
 }
 
 popup.addEventListener('click', function (e) {
-    if (e.target === e.currentTarget) {
-        closePopup();
-    }
-})
-popupCard.addEventListener('click', function (e) {
     if (e.target === e.currentTarget) {
         closePopup();
     }
@@ -75,11 +30,6 @@ function formSubmitHandler(evt) {
     profileDescription.textContent = jobInput.value;
     closePopup();
 }
-
-
-cardAddOpenPopupButton.addEventListener('click', openPopupCard);
-cardClosePopupButton.addEventListener('click', closePopup);
-
 
 profileOpenPopupButton.addEventListener('click', openPopup);
 popupCloseButton.addEventListener('click', closePopup);
